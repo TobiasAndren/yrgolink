e18edafc83f2eb7cb3c9a3afbe4c1242f8644aa0
Sandra <sandra0924@protonmail.com>
2025-04-08 23:05:23 +0200

    added page to display companies, fetched data from db and added button with mailto functionality

---
+++ app/registered-companies/[id]/page.jsx
@@ -0,0 +1,80 @@
+'use client'
+import { Button } from '@/app/components/form-components/Button';
+import { useEffect, useState } from 'react';
+import { useParams } from 'next/navigation';
+import { createClient } from '@/utils/supabase/client';
+
+export default function CompanyDetails() {
+  const [company, setCompany] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const supabase = createClient();
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    if (!id) {
+      console.log("No ID in URL");
+      return; // Tidig return om ID inte finns
+    }
+
+    const fetchCompanyDetails = async () => {
+      try {
+        setLoading(true); // Sätt loading till true
+        console.log("Fetching company details for ID:", id);
+
+        const { data, error } = await supabase
+          .from('companies')
+          .select('*')
+          .eq('id', id)
+          .single(); // Hämta en enda rad (företag)
+
+        if (error) {
+          throw error;
+        }
+
+        console.log("Fetched data:", data);
+        setCompany(data);
+      } catch (error) {
+        console.error("Error:", error);  // Logga eventuella fel
+        setError('Failed to load company details');
+      } finally {
+        setLoading(false); // Sätt loading till false
+      }
+    };
+
+    fetchCompanyDetails();
+  }, [id]);  // Kör denna effekt när id förändras
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
+
+  return (
+    <section>
+        <h2>Företagsinformation</h2>
+        <div>
+            <h4>{company?.name || 'Företagsnamn saknas'}</h4>
+            <p>{company?.website ? company.website.replace(/^https?:\/\//, '') : 'Hemsida saknas'}</p>
+        </div>
+        <div>
+            <h4>Kontaktperson</h4>
+            <p>{company?.contact_name || 'Namn saknas'}</p>
+            <p>{company?.contact_email || 'E-post saknas'}</p>
+        </div>
+        <div>
+            <h4>Om företaget</h4>
+            <p>{company?.description || 'Beskrivning saknas'}</p>
+        </div>
+        <div><strong>Arbetsform</strong>: {company?.employment_mode || 'Uppgift saknas'}</div>
+        
+        {company?.contact_email && (
+            <Button
+            text="Skicka anmälan"
+            textColor="white"
+            backgroundColor="red"
+            onClick={() => window.location.href = `mailto:${company.contact_email}`}
+          />
+        )}
+    </section>
+  );
+}
---
+++ app/registered-companies/page.jsx
@@ -0,0 +1,48 @@
+'use client'
+import { useEffect, useState } from 'react';
+import { createClient } from '@/utils/supabase/client';
+import Link from 'next/link';
+
+export default function RegisteredCompanies() {
+  const [companies, setCompanies] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const supabase = createClient();
+
+  useEffect(() => {
+    const fetchCompanies = async () => {
+      try {
+        // Hämta data från Supabase
+        const { data, error } = await supabase.from('companies').select('*');
+        if (error) {
+          throw error;
+        }
+        setCompanies(data);
+      } catch (error) {
+        setError('Failed to load companies');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCompanies();
+  }, []); // Tom array betyder att detta körs en gång vid laddning
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
+
+  return (
+    <section>
+      <h2>Registered Companies</h2>
+      <ul className="list">
+        {companies.map((company) => (
+        <li key={company.id}>
+          <Link href={`/registered-companies/${company.id}`}>
+              {company.name}
+          </Link>
+        </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
