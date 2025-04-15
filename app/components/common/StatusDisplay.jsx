77ccb068c25c61f136af161f538e8b76d3229b14
TobiasAndren <tobiasandren@hotmail.com>
2025-04-15 12:11:06 +0200

    added a statusdisplay component

---
+++ app/components/common/StatusDisplay.jsx
@@ -0,0 +1,50 @@
+"use client";
+
+import styled from "@emotion/styled";
+
+const StyledStatus = styled.p(
+  ({ isError }) => `
+  background-color: ${isError ? `#FFBABA` : `#DFF2BF`};
+  color: ${isError ? `red` : `green`};
+  align-self: center;
+  width: 90%;
+  border-radius: 0.75rem;
+  padding: 0.75rem;
+  display: flex;
+  align-items: center;
+  font-weight: 400;
+  gap: 1rem;
+  border: 1px solid ${isError ? `red` : `green`};
+  height: 3rem;
+  margin-bottom: 2.5rem;
+
+  img {
+    height: 1.25rem;
+  }
+
+  @media (min-width: 600px) {
+    width: 80%;
+  }
+
+  @media (min-width: 768px) {
+    width: 70%;
+  }
+
+  @media (min-width: 1200px) {
+    width: 50%;
+  }
+`
+);
+
+export function StatusDisplay({ isError, children }) {
+  return (
+    <StyledStatus isError={isError}>
+      {isError ? (
+        <img src="red-x.svg" alt="" />
+      ) : (
+        <img src="green-check.svg" alt="" />
+      )}
+      {children}
+    </StyledStatus>
+  );
+}
